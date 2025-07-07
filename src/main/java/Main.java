f6b211d320a96768ce6fc83d72147bb1c2e1d7ab
724thomas <724thomas@gmail.com>
2025-07-07 11:18:02 +0900

    feat: Read a blob object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,6 @@ import java.util.zip.Inflater;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        System.err.println("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        //
         final String command = args[0];
 
         switch (command) {
@@ -30,7 +25,6 @@ public class Main {
                     throw new RuntimeException(e);
                 }
             }
-
             case "cat-file" -> {
                 final String objectHash = args[2];
                 final String objectFolder = objectHash.substring(0, 2);
@@ -47,17 +41,15 @@ public class Main {
                             outputStream.write(buffer, 0, count);
                         }
                         String decompressedString = outputStream.toString("UTF-8");
-                        System.out.println(decompressedString.substring(decompressedString.indexOf('\0') + 1));
+                        System.out.print(decompressedString.substring(decompressedString.indexOf("\0") + 1));
 
                     } catch (DataFormatException e) {
                         throw new RuntimeException(e);
                     }
-
                 } catch (IOException e) {
-                    System.err.println("Error reading object: " + e.getMessage());
+                    throw new RuntimeException(e);
                 }
             }
-
             default -> System.out.println("Unknown command: " + command);
         }
     }
