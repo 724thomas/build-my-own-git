0c077c331d9b55c01c6b4e035d5b1fe284db69b1
724thomas <724thomas@gmail.com>
2025-07-07 11:13:24 +0900

    feat: Read a blob object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.DataFormatException;
+import java.util.zip.Inflater;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,6 +30,34 @@ public class Main {
                     throw new RuntimeException(e);
                 }
             }
+
+            case "cat-file" -> {
+                final String objectHash = args[2];
+                final String objectFolder = objectHash.substring(0, 2);
+                final String objectFilename = objectHash.substring(2);
+                try {
+                    byte[] data = Files.readAllBytes(Paths.get(".git/objects/" + objectFolder + "/" + objectFilename));
+                    Inflater inflater = new Inflater();
+                    inflater.setInput(data);
+
+                    try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length)) {
+                        byte[] buffer = new byte[1024];
+                        while (!inflater.finished()) {
+                            int count = inflater.inflate(buffer);
+                            outputStream.write(buffer, 0, count);
+                        }
+                        String decompressedString = outputStream.toString("UTF-8");
+                        System.out.println(decompressedString.substring(decompressedString.indexOf('\0') + 1));
+
+                    } catch (DataFormatException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                } catch (IOException e) {
+                    System.err.println("Error reading object: " + e.getMessage());
+                }
+            }
+
             default -> System.out.println("Unknown command: " + command);
         }
     }
